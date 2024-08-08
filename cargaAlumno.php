85c166fc01b33381739c4671ea44c53cd2b298a1
Juanshu05 <jc.aponteflorentin@gmail.com>
2024-08-08 09:46:48 -0300

    prueba de actualizacion

---
+++ cargaAlumno.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form>
+        </form method="POST" action="cargaEstudiantes.php">
+            <input type="text" name="cuil alumno" placeholder="CUIL_Alumno">
+            <input type="submit">
+        </form>
+    </body>
+</html>
--- cargaEstudiantes.php
+++ cargaEstudiantes.php
@@ -3,7 +3,10 @@
 
 	$con = mysqli_connect($host,$user,$pwd,$db) or die("Fallo de conexión");
 
-	$query = "insert into alumnos (ID_ALUMNO, DNI, cedula, nombre, legajo, libro, folio, fecha_Nacimiento, hora_Nacimiento, localidad_Nacimiento, localidad, municipio, provincia, nacionalidad, nombre_Calle, numero_Casa, piso, numero_Departamento) values ('$_POST[ID_ALUMNO]', '$_POST[DNI]', '$_POST[cedula]', '$_POST[nombre]', '$_POST[legajo]', '$_POST[libro]',  '$_POST[folio]', '$_POST[fecha_Nacimiento]', '$_POST[hora_Nacimiento]', '$_POST[localidad_Nacimiento]', '$_POST[localidad]', '$_POST[municipio]', '$_POST[provincia]','$_POST[nacionalidad]', '$_POST[nombre_Calle]', '$_POST[numero_Casa]', '$_POST[piso]', '$_POST[numero_Departamento]')";
+	$query = "insert into alumno 
+	(CUIL) 
+	values
+	('$_POST[CUIL_Alumno]')";
 
 	mysqli_query($con, $query) or die ("Fallo de consulta");
 	echo "datos cargados";
